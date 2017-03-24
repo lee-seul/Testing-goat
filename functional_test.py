06344f06c9ad7f2bd25dfbebf714d6447687a722
seul lee <blacksangi14@naver.com>
2017-03-24 20:17:50 +0900

    템플릿을 이용한 메인 페이지 HTML 생성

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -18,17 +18,17 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('To Do', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To DO', header_text)
+        self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attribute('placeholde'),
-                '작업 아이템 입력'
+                inputbox.get_attribute('placeholder'),
+                '작업 아이템 입력')
         
         inputbox.send_keys('공작깃털 사기')
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')

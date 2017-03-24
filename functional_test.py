942d05ef7f7adc6859cbe0398fe8026ef9af7d30
seul lee <blacksangi14@naver.com>
2017-03-24 23:09:06 +0900

    functional_test.py, home.html, tests.py, views.py 변경 내용 표시

--- functional_test.py
+++ functional_test.py
@@ -30,11 +30,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('공작깃털 사기')
         inputbox.send_keys(Keys.ENTER)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys(Keys.ENTER)
+
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: 공작깃털 사기' for row in rows),
-            )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn('2: 공작깃털을 이용해서 그물 만들기', 
+                [row.text for row in rows])
         
         self.fail('Finish the test!')
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -16,4 +18,16 @@ class HomePageTest(TestCase):
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
 
+    def tset_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+        
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+                'home.html',
+                {'new_item_text': '신규 작업 아이템'})
+        self.assertEqual(response.content.decode(), expected_html)
 

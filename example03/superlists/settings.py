b7aaef0594aa7988a905fa034d9a26626f75ce9d
seul lee <blacksangi14@naver.com>
2017-04-02 15:35:09 +0900

    템플릿을 이용하도록 홈페이지 뷰를 리팩토링

--- example03/superlists/settings.py
+++ example03/superlists/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'lists',
+    ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',

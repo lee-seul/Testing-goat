5c9c77c3ea9769d363c8a20ce367d804e877a1fa
seul lee <blacksangi14@naver.com>
2017-04-02 20:30:22 +0900

    url 분리

--- example03/superlists/urls.py
+++ example03/superlists/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name = 'view_list'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^lists/', include('lists.urls')),
 
     url(r'^admin/', admin.site.urls),
     ]

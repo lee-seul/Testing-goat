c1611549b1fba04778f116d62bc16eed4854cda1
seul lee <blacksangi14@naver.com>
2017-04-02 16:28:40 +0900

    functional_test.py, home.html, tests.py, views.py 변경 내용 표시

--- example03/lists/tests.py
+++ example03/lists/tests.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.shortcuts import render 
 
 from lists.views import home_page
 
@@ -17,6 +18,22 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_response = render(request, 'home.html')
+        self.assertEqual(response.content.decode(), 
+                         expected_response.content.decode())
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+
+        expected_response = render(request, 'home.html', 
+                                   {'new_item_text': '신규 작업 아이템'}
+                                   )
+        self.assertEqual(response.content.decode(),
+                         expected_response.content.decode())
 
